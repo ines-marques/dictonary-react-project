3ea21a12d40124caf6d8d1ba98bb92028a438af9
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-07-04 13:08:45 +0100

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Inês Marques and is {" "}
+            <a href="https://github.com/ines-marques/dictonary-react-project" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
